183d3340218d148be87f715e54e584ce312aa1d3
Ollocreate <neustroeva-olenka@mail.ru>
2023-12-03 20:11:57 +0300

    Added BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 // TODO: Проверка на наличие интернета, splash скрин (во время загрузки проверка авторизации);
+// TODO: По нажатию на фотку кота с помощью hero animation должна открывать фотка на весь экран (в виде нового экрана)
 
 void main() {
   runApp(MyApp());
@@ -13,155 +12,117 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Cat Tinder',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
-        ),
-        home: MyHomePage(),
-      ),
+    return MaterialApp(
+      home: MyHomePage(),
     );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  // TODO: Сделать парсинг из API, при нажатии на фотографию, она открывается на весь экран с hero animation;
-
-  var current = WordPair.random();
-
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  var favorites = <WordPair>[];
-
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-    notifyListeners();
   }
 }
 
 class MyHomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  var selectedIndex = 0;
+  int _selectedIndex = 0;
+  
+  final List<Widget> _pages = <Widget>[
+    GeneratorPage(),
+    FavoritePage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    var colorScheme = Theme.of(context).colorScheme;
-
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = GeneratorPage();
-      case 1:
-        page = FavoritePage();
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
-
-    var mainArea = ColoredBox(
-        color: colorScheme.surfaceVariant,
-        child: AnimatedSwitcher(
-          duration: Duration(milliseconds: 200),
-          child: page,
-        ),
-    );
-
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-            body: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cat tinder'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ElevatedButton(
+// TODO: Сделать выход из системы
+                onPressed: () {},
+                child: Text('Quit')),
+          )
+        ],
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SafeArea(
+            child: Column(
               children: [
                 Expanded(
-                    child: Container(
-                      color: Theme.of(context).colorScheme.primaryContainer,
-                      child: page,
-                    )
-                ),
-                BottomAppBar(
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.home),
-                            onPressed: () {
-                              setState(() {
-                                _onTabSelected(0);
-                              });
-                            },
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.favorite),
-                            onPressed: () {
-                              setState(() {
-                                _onTabSelected(1);
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              );
-            }
+                  child: IndexedStack(
+                    index: _selectedIndex,
+                    children: _pages,
+                  )
+                )
+              ],
+            ),
           );
-        }
-        void _onTabSelected(int index) {
-          setState(() {
-            selectedIndex = index;
-        }
-      );
-    }
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.indigo,
+        iconSize: 30,
+        mouseCursor: SystemMouseCursors.click,
+        selectedItemColor: Colors.white,
+        selectedLabelStyle: TextStyle(
+          fontWeight: FontWeight.bold
+        ),
+        showUnselectedLabels: false,
+
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorites'),
+          ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
   }
+
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
 
 class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
+    // IconData icon;
+    // if (appState.favorites.contains(pair)) {
+    //   icon = Icons.favorite;
+    // } else {
+    //   icon = Icons.favorite_border;
+    // }
+// TODO: Загрузка котов через API
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          BigCard(pair: pair),
           SizedBox(height: 10),
           Row(
             mainAxisSize: MainAxisSize.min,
             children: [
               ElevatedButton.icon(
                   onPressed: () {
-                    appState.toggleFavorite();
+                    // appState.toggleFavorite();
                   },
-                  icon: Icon(icon),
+                  icon: Icon(Icons.favorite),
                   label: Text('Like'),
               ),
               SizedBox(width: 10),
               ElevatedButton(
                   onPressed: () {
-                    appState.getNext();
+                    // appState.getNext();
                   },
                   child: Text('Next'),
               ),
@@ -176,27 +137,25 @@ class GeneratorPage extends StatelessWidget {
 class FavoritePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+    // var appState = context.watch<MyAppState>();
 
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
+    // if (appState.favorites.isEmpty) {
+    //   return Center(
+    //     child: Text('No favorites yet.'),
+    //   );
+    // }
 
     return ListView(
       children: [
         Padding(
             padding: const EdgeInsets.all(20),
-            child: Text('You have '
-            '${appState.favorites.length} favorites:'),
+            child: Text('You have 0 favorites:'),
         ),
-        for (var pair in appState.favorites)
-        // TODO: Сделать сохранение локально sharedPreferences;
+        // for (var pair in appState.favorites)
+        // TODO: В избранное добавляется при нажатии лайка Сделать сохранение локально sharedPreferences;
 
           ListTile(
             leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
           ),
       ],
     );
@@ -205,31 +164,3 @@ class FavoritePage extends StatelessWidget {
 
 // TODO: Добавить AuthorizationPage, сделать сохранение пользователя локально sharedPreferences;
 
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(
-            pair.asLowerCase,
-            style: style,
-            semanticsLabel: "${pair.first} ${pair.second}",
-        ),
-      ),
-    );
-  }
-}
